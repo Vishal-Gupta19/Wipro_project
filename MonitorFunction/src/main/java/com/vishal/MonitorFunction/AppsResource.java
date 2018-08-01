9eee2daecc39138d44e81fe46985e14f41ae4b39
Vishal Gupta <vishalgupta9596@gmail.com>
2018-08-01 23:37:51 +0530

    Test Timer added

--- MonitorFunction/src/main/java/com/vishal/MonitorFunction/AppsResource.java
+++ MonitorFunction/src/main/java/com/vishal/MonitorFunction/AppsResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -13,6 +14,14 @@ import javax.ws.rs.core.MediaType;
 @Path("apps")
 public class AppsResource {
 	AppRepository repo = new AppRepository();
+	Heartbeat delay=new Heartbeat();
+	
+	public void main(String[] args) {
+		
+		delay.completeTask();
+		System.out.println("Reading incoming request...");
+	}
+	
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -29,13 +38,32 @@ public class AppsResource {
 		return repo.getApp(id);
 	}
 	
-	@POST
-	@Path("post")
+	@PUT
+	@Path("put/{timestamp}/{type}/{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public App createApp(App a1) {
-		
+	public App updateApp(
+			@PathParam("timestamp")String timestamp,
+			@PathParam("type")String type,
+			@PathParam("id")int id) {
+		App a1=new App();;
+		a1.setId(id);
+		a1.setTimestamp(timestamp);
+		a1.setType(type);
 		System.out.println(a1);
-		repo.create(a1);
+		if(repo.getApp(a1.getId()).getId()==0) {
+			repo.create(a1);
+		}
+		repo.update(a1);
 		return a1;
 	}
+	
+//	@POST
+//	@Path("post")
+//	@Consumes(MediaType.APPLICATION_JSON)
+//	public App createApp(App a1) {
+//		
+//		System.out.println(a1);
+//		repo.create(a1);
+//		return a1;
+//	}
 }
